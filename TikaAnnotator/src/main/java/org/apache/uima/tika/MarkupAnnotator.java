8616965d9f752a15650a423014e3e66c44433407
Kamil Podlesak <kamil.podlesak@lmc.eu>
2016-10-06 17:03:04 +0200

    core reformat: recommended Java format

--- TikaAnnotator/src/main/java/org/apache/uima/tika/MarkupAnnotator.java
+++ TikaAnnotator/src/main/java/org/apache/uima/tika/MarkupAnnotator.java
@@ -50,157 +50,153 @@ import java.util.Iterator;
 public class MarkupAnnotator extends CasAnnotator_ImplBase {
 
 
-  private final static String ORIGINAL_VIEW_PARAM_NAME = "ORIGINAL_VIEW_PARAM_NAME";
-  private final static String TEXT_VIEW_PARAM_NAME = "TEXT_VIEW_PARAM_NAME";
+    private final static String ORIGINAL_VIEW_PARAM_NAME = "ORIGINAL_VIEW_PARAM_NAME";
+    private final static String TEXT_VIEW_PARAM_NAME = "TEXT_VIEW_PARAM_NAME";
 
-  private final static String tika_file_param = "tikaConfigFile";
+    private final static String tika_file_param = "tikaConfigFile";
 
-  // takes an option indicating the name of the view containing the binary document
-  private String originalViewName = CAS.NAME_DEFAULT_SOFA;
+    // takes an option indicating the name of the view containing the binary document
+    private String originalViewName = CAS.NAME_DEFAULT_SOFA;
 
-  // takes an option indicating the name of the view containing the text version of the document
-  private String textViewName = "textView";
+    // takes an option indicating the name of the view containing the text version of the document
+    private String textViewName = "textView";
 
-  // configuration for TIKA - can be created by specifying a custom resource
-  private TikaConfig config = null;
+    // configuration for TIKA - can be created by specifying a custom resource
+    private TikaConfig config = null;
 
-  public void initialize(UimaContext aContext) throws ResourceInitializationException {
-    super.initialize(aContext);
-    // Get config param setting
-    originalViewName = (String) aContext.getConfigParameterValue(ORIGINAL_VIEW_PARAM_NAME);
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext);
+        // Get config param setting
+        originalViewName = (String) aContext.getConfigParameterValue(ORIGINAL_VIEW_PARAM_NAME);
 
-    textViewName = (String) aContext.getConfigParameterValue(TEXT_VIEW_PARAM_NAME);
-    if (textViewName == null) {
-      this.getContext().getLogger().log(Level.WARNING,
-              new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is null; setting to \"textView\"").toString());
-      textViewName = "textView";
-    } else
-      this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is ")
-              .append(textViewName).toString());
+        textViewName = (String) aContext.getConfigParameterValue(TEXT_VIEW_PARAM_NAME);
+        if (textViewName == null) {
+            this.getContext().getLogger().log(Level.WARNING,
+                    new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is null; setting to \"textView\"").toString());
+            textViewName = "textView";
+        } else
+            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is ")
+                    .append(textViewName).toString());
 
-    // initialise TIKA parser
-    // try to get a custom config
-    URL tikaConfigURL = null;
-    try {
-      tikaConfigURL = getContext().getResourceURL(tika_file_param);
-      config = new TikaConfig(tikaConfigURL);
-    } catch (Exception e1) {
-      // to log
-      this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Failed to load TIKA config file from ")
-              .append(tikaConfigURL).append(" due to ").append(e1.getLocalizedMessage()).toString());
-      config = null;
+        // initialise TIKA parser
+        // try to get a custom config
+        URL tikaConfigURL = null;
+        try {
+            tikaConfigURL = getContext().getResourceURL(tika_file_param);
+            config = new TikaConfig(tikaConfigURL);
+        } catch (Exception e1) {
+            // to log
+            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Failed to load TIKA config file from ")
+                    .append(tikaConfigURL).append(" due to ").append(e1.getLocalizedMessage()).toString());
+            config = null;
+        }
+
+        // if not rely on default one
+        if (config == null) {
+            try {
+                config = TikaConfig.getDefaultConfig();
+            } catch (Exception e) {
+                throw new ResourceInitializationException(e);
+            }
+        }
+
     }
 
-    // if not rely on default one
-    if (config == null) {
-      try {
-        config = TikaConfig.getDefaultConfig();
-      } catch (Exception e) {
-        throw new ResourceInitializationException(e);
-      }
-    }
+    public void process(CAS cas) throws AnalysisEngineProcessException {
+        CAS originalCas = null;
+        try {
+            originalCas = cas.getView(originalViewName);
+        } catch (Exception e) {
+            String viewName = cas.getViewName();
+            // can't find originalViewName
+            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("can't find view ").append(originalViewName)
+                    .append(" using ").append(viewName).append(" instead").toString());
+            originalCas = cas.getCurrentView();
+        }
 
-  }
+        InputStream originalStream = originalCas.getSofa().getSofaDataStream();
 
-  public void process(CAS cas) throws AnalysisEngineProcessException {
-    CAS originalCas = null;
-    try {
-      originalCas = cas.getView(originalViewName);
-    }
-    catch (Exception e) {
-      String viewName = cas.getViewName();
-      // can't find originalViewName
-      this.getContext().getLogger().log(Level.WARNING, new StringBuffer("can't find view ").append(originalViewName)
-              .append(" using ").append(viewName).append(" instead").toString());
-      originalCas = cas.getCurrentView();
-    }
+        // parsing with TIKA
 
-    InputStream originalStream = originalCas.getSofa().getSofaDataStream();
+        // TODO if content type is known then we use it
+        // otherwise we guess
 
-    // parsing with TIKA
+        Parser parser = new AutoDetectParser(config);
 
-    // TODO if content type is known then we use it
-    // otherwise we guess
+        Metadata md = new Metadata();
+        MarkupHandler handler = new MarkupHandler();
+        final ParseContext parseContext = new ParseContext();
 
-    Parser parser = new AutoDetectParser(config);
+        try {
+            parser.parse(originalStream, handler, md, parseContext);
+        } catch (Exception e) {
+            // if we have a problem just dump the message and continue
+            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Problem converting file : ")
+                    .append(e.getMessage()).toString());
+            // PROBLEM => trying to serialize binary content in XML crash!
+            return;
+        } finally {
+            try {
+                originalStream.close();
+            } catch (IOException e) {
+                //ignored
+            }
+        }
 
-    Metadata md = new Metadata();
-    MarkupHandler handler = new MarkupHandler();
-    final ParseContext parseContext = new ParseContext();
-
-    try {
-      parser.parse(originalStream, handler, md, parseContext);
-    }
-    catch (Exception e) {
-      // if we have a problem just dump the message and continue
-      this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Problem converting file : ")
-              .append(e.getMessage()).toString());
-      // PROBLEM => trying to serialize binary content in XML crash!
-      return;
-    }
-    finally {
-      try {
-        originalStream.close();
-      } catch (IOException e) {
-        //ignored
-      }
-    }
-
-    CAS plainTextView = cas.createView(textViewName);
+        CAS plainTextView = cas.createView(textViewName);
 
 
-    handler.populateCAS(plainTextView);
+        handler.populateCAS(plainTextView);
 
-    // get additional metadata about the document
-    // e.g content type etc...
-    // TODO add possibility to define type as parameter and discover
-    // feature names on the fly
-    JCas ptv = null;
-    try {
-      ptv = plainTextView.getJCas();
-    } catch (CASException e) {
-      e.printStackTrace();
-      return;
-    }
+        // get additional metadata about the document
+        // e.g content type etc...
+        // TODO add possibility to define type as parameter and discover
+        // feature names on the fly
+        JCas ptv = null;
+        try {
+            ptv = plainTextView.getJCas();
+        } catch (CASException e) {
+            e.printStackTrace();
+            return;
+        }
     /* identify language */
-    extractLanguage(ptv);
+        extractLanguage(ptv);
 
-    Type docAnnotationType = ptv.getTypeSystem().getType("org.apache.uima.tika.SourceDocumentAnnotation");
-    Iterator iter = ptv.getAnnotationIndex(docAnnotationType).iterator();
-    SourceDocumentAnnotation docAnnotation = null;
-    // do we already have one?
-    if (iter.hasNext()) docAnnotation = (SourceDocumentAnnotation) iter.next();
-      // otherwise let's create a new annotation
-    else docAnnotation = new SourceDocumentAnnotation(ptv);
+        Type docAnnotationType = ptv.getTypeSystem().getType("org.apache.uima.tika.SourceDocumentAnnotation");
+        Iterator iter = ptv.getAnnotationIndex(docAnnotationType).iterator();
+        SourceDocumentAnnotation docAnnotation = null;
+        // do we already have one?
+        if (iter.hasNext()) docAnnotation = (SourceDocumentAnnotation) iter.next();
+            // otherwise let's create a new annotation
+        else docAnnotation = new SourceDocumentAnnotation(ptv);
 
-    // now iterate on the metadata found by Tika and add them to the info
-    if (docAnnotation.getFeatures() == null)
-      docAnnotation.setFeatures((FSArray) cas.createArrayFS(md.size()));
+        // now iterate on the metadata found by Tika and add them to the info
+        if (docAnnotation.getFeatures() == null)
+            docAnnotation.setFeatures((FSArray) cas.createArrayFS(md.size()));
 
-    for (int i = 0; i < md.size(); i++) {
-      String name = md.names()[i];
-      String value = md.get(name);
-      FeatureValue fv = new FeatureValue(ptv);
-      fv.setName(name);
-      fv.setValue(value);
-      docAnnotation.setFeatures(i, fv);
+        for (int i = 0; i < md.size(); i++) {
+            String name = md.names()[i];
+            String value = md.get(name);
+            FeatureValue fv = new FeatureValue(ptv);
+            fv.setName(name);
+            fv.setValue(value);
+            docAnnotation.setFeatures(i, fv);
+        }
+        docAnnotation.addToIndexes();
+
     }
-    docAnnotation.addToIndexes();
 
-  }
-
-  private void extractLanguage(JCas plainTextView) {
-    try {
-      LanguageIdentifier li = new LanguageIdentifier(new LanguageProfile(plainTextView.getDocumentText()));
-      if (li.getLanguage() != null && !"".equals(li.getLanguage()))
-        plainTextView.setDocumentLanguage(li.getLanguage());
+    private void extractLanguage(JCas plainTextView) {
+        try {
+            LanguageIdentifier li = new LanguageIdentifier(new LanguageProfile(plainTextView.getDocumentText()));
+            if (li.getLanguage() != null && !"".equals(li.getLanguage()))
+                plainTextView.setDocumentLanguage(li.getLanguage());
+        } catch (Exception e) {
+            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Could not extract language due to ")
+                    .append(e.getLocalizedMessage()).toString());
+        }
+        this.getContext().getLogger().log(Level.INFO, new StringBuffer("Extracted language: ").append(plainTextView
+                .getDocumentLanguage()).toString());
     }
-    catch (Exception e) {
-      this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Could not extract language due to ")
-              .append(e.getLocalizedMessage()).toString());
-    }
-    this.getContext().getLogger().log(Level.INFO,new StringBuffer("Extracted language: ").append(plainTextView
-            .getDocumentLanguage()).toString());
-  }
 
 }
