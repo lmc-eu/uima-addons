7d747674e4e1943461b4ef63e146541f003d08ac
Kamil Podlesak <kamil.podlesak@lmc.eu>
2016-10-06 18:58:13 +0200

    MarkupAnnotator now uses common implementation
    no more copy&paste!

--- TikaAnnotator/src/main/java/org/apache/uima/tika/MarkupAnnotator.java
+++ TikaAnnotator/src/main/java/org/apache/uima/tika/MarkupAnnotator.java
@@ -19,28 +19,19 @@
 
 package org.apache.uima.tika;
 
-import org.apache.tika.config.TikaConfig;
 import org.apache.tika.language.LanguageIdentifier;
 import org.apache.tika.language.LanguageProfile;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.parser.ParseContext;
-import org.apache.tika.parser.Parser;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.CasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
-import org.apache.uima.cas.Type;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Level;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.Iterator;
 
 
 /**
@@ -53,57 +44,39 @@ public class MarkupAnnotator extends CasAnnotator_ImplBase {
     private final static String ORIGINAL_VIEW_PARAM_NAME = "ORIGINAL_VIEW_PARAM_NAME";
     private final static String TEXT_VIEW_PARAM_NAME = "TEXT_VIEW_PARAM_NAME";
 
-    private final static String tika_file_param = "tikaConfigFile";
-
     // takes an option indicating the name of the view containing the binary document
     private String originalViewName = CAS.NAME_DEFAULT_SOFA;
 
     // takes an option indicating the name of the view containing the text version of the document
     private String textViewName = "textView";
 
-    // configuration for TIKA - can be created by specifying a custom resource
-    private TikaConfig config = null;
+    // Tika wrapper
+    private TIKAWrapper tika;
+
+
+    private String getConfigValue(UimaContext aContext, String paramName, String defaultValue) {
+        final String value = (String) aContext.getConfigParameterValue(paramName);
+        if (value == null || value.isEmpty()) {
+            return defaultValue;
+        }
+        return value;
+    }
 
     public void initialize(UimaContext aContext) throws ResourceInitializationException {
         super.initialize(aContext);
         // Get config param setting
-        originalViewName = (String) aContext.getConfigParameterValue(ORIGINAL_VIEW_PARAM_NAME);
+        originalViewName = getConfigValue(aContext, ORIGINAL_VIEW_PARAM_NAME, CAS.NAME_DEFAULT_SOFA);
 
-        textViewName = (String) aContext.getConfigParameterValue(TEXT_VIEW_PARAM_NAME);
-        if (textViewName == null) {
-            this.getContext().getLogger().log(Level.WARNING,
-                    new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is null; setting to \"textView\"").toString());
-            textViewName = "textView";
-        } else
-            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Parameter TEXT_VIEW_PARAM_NAME is ")
-                    .append(textViewName).toString());
+        textViewName = getConfigValue(aContext, TEXT_VIEW_PARAM_NAME, "textView");
 
         // initialise TIKA parser
         // try to get a custom config
-        URL tikaConfigURL = null;
-        try {
-            tikaConfigURL = getContext().getResourceURL(tika_file_param);
-            config = new TikaConfig(tikaConfigURL);
-        } catch (Exception e1) {
-            // to log
-            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Failed to load TIKA config file from ")
-                    .append(tikaConfigURL).append(" due to ").append(e1.getLocalizedMessage()).toString());
-            config = null;
-        }
-
-        // if not rely on default one
-        if (config == null) {
-            try {
-                config = TikaConfig.getDefaultConfig();
-            } catch (Exception e) {
-                throw new ResourceInitializationException(e);
-            }
-        }
-
+        // initialise TIKA parser
+        tika = TIKAWrapperFactory.createTika(aContext);
     }
 
     public void process(CAS cas) throws AnalysisEngineProcessException {
-        CAS originalCas = null;
+        CAS originalCas;
         try {
             originalCas = cas.getView(originalViewName);
         } catch (Exception e) {
@@ -114,76 +87,25 @@ public class MarkupAnnotator extends CasAnnotator_ImplBase {
             originalCas = cas.getCurrentView();
         }
 
-        InputStream originalStream = originalCas.getSofa().getSofaDataStream();
-
-        // parsing with TIKA
-
         // TODO if content type is known then we use it
         // otherwise we guess
 
-        Parser parser = new AutoDetectParser(config);
-
-        Metadata md = new Metadata();
-        MarkupHandler handler = new MarkupHandler();
-        final ParseContext parseContext = new ParseContext();
-
-        try {
-            parser.parse(originalStream, handler, md, parseContext);
-        } catch (Exception e) {
-            // if we have a problem just dump the message and continue
-            this.getContext().getLogger().log(Level.WARNING, new StringBuffer("Problem converting file : ")
-                    .append(e.getMessage()).toString());
-            // PROBLEM => trying to serialize binary content in XML crash!
-            return;
-        } finally {
-            try {
-                originalStream.close();
-            } catch (IOException e) {
-                //ignored
-            }
+        CAS plainTextView = cas.createView(textViewName);
+        try (InputStream is = originalCas.getSofaDataStream()) {
+            tika.populateCASfromURL(plainTextView, is, null, null, null);
+        } catch (IOException | CASException e) {
+            throw new AnalysisEngineProcessException(e);
         }
 
-        CAS plainTextView = cas.createView(textViewName);
-
-
-        handler.populateCAS(plainTextView);
-
-        // get additional metadata about the document
-        // e.g content type etc...
-        // TODO add possibility to define type as parameter and discover
-        // feature names on the fly
-        JCas ptv = null;
+        JCas ptv;
         try {
             ptv = plainTextView.getJCas();
         } catch (CASException e) {
-            e.printStackTrace();
-            return;
+            //this should never happen...
+            throw new AnalysisEngineProcessException(e);
         }
     /* identify language */
         extractLanguage(ptv);
-
-        Type docAnnotationType = ptv.getTypeSystem().getType("org.apache.uima.tika.SourceDocumentAnnotation");
-        Iterator iter = ptv.getAnnotationIndex(docAnnotationType).iterator();
-        SourceDocumentAnnotation docAnnotation = null;
-        // do we already have one?
-        if (iter.hasNext()) docAnnotation = (SourceDocumentAnnotation) iter.next();
-            // otherwise let's create a new annotation
-        else docAnnotation = new SourceDocumentAnnotation(ptv);
-
-        // now iterate on the metadata found by Tika and add them to the info
-        if (docAnnotation.getFeatures() == null)
-            docAnnotation.setFeatures((FSArray) cas.createArrayFS(md.size()));
-
-        for (int i = 0; i < md.size(); i++) {
-            String name = md.names()[i];
-            String value = md.get(name);
-            FeatureValue fv = new FeatureValue(ptv);
-            fv.setName(name);
-            fv.setValue(value);
-            docAnnotation.setFeatures(i, fv);
-        }
-        docAnnotation.addToIndexes();
-
     }
 
     private void extractLanguage(JCas plainTextView) {
